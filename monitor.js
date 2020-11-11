2158ca5ba9f7b6d25258fd4f3fdcaca70f1226b5
Raakka <jrip03@gmail.com>
2020-11-10 23:12:44 -0500

    more vars

--- monitor.js
+++ monitor.js
@@ -9,9 +9,40 @@ var options = {
   }
 };
 
+let msg = `{
+  "embeds": [
+    {
+      "title": "New Edpuzzle",
+      "fields": [
+        {
+          "name": "Assignment Id:",
+          "value": "null"
+        }
+      ],
+      "footer": {
+        "text": "Powered by XVI#0016"
+      }
+    }
+  ]
+}`;
+
+let whurls = [];
+whurls = process.env.WEBHOOK;
+
+function post2webhook(jsonmsg) {
+  whurls.forEach((whurl, i) => {
+    fetch(whurl + "?wait=true",
+    {"method":"POST",
+    "headers": {"content-type": "application/json"},
+    "body": JSON.stringify(jsonmsg)})
+    .then(a=>a.json()).then(console.log)
+  });
+}
+
 setInterval(function(){
   request(options, function (error, response) {
     if (error) throw new Error(error);
     let lolBigObj = JSON.parse(response.body);
+    lolBigObj
     console.log("ping!");
   })}, 15000);
